02a0f88c93cda1e2834e90eaff24803b72e2d24c
Honggang Li <honggangli@163.com>
2016-09-21 09:11:05 +0800

    Update build tool
    Signed-off-by: Honggang Li <honggangli@163.com>

--- tools/build.c
+++ tools/build.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>	/* unistd.h needs this */
 #include <unistd.h>	/* contains read/write */
 #include <fcntl.h>
+#include <string.h>
 
 #define MINIX_HEADER 32
 #define GCC_HEADER 1024
@@ -20,11 +21,14 @@ void usage(void)
 
 int main(int argc, char ** argv)
 {
-	int i,c,id;
+	int i,c,id,pading=0;
 	char buf[1024];
 
 	if (argc != 3)
 		usage();
+
+	memset(buf, 0, 1024);
+
 	for (i=0;i<sizeof buf; i++) buf[i]=0;
 	if ((id=open(argv[1],O_RDONLY,0))<0)
 		die("Unable to open 'boot'");
@@ -44,8 +48,9 @@ int main(int argc, char ** argv)
 		die("Illegal symbol table in 'boot'");
 	i=read(id,buf,sizeof buf);
 	fprintf(stderr,"Boot sector %d bytes.\n",i);
-	if (i>510)
-		die("Boot block may not exceed 510 bytes");
+	fprintf(stderr, "Boot block is %d bytes.", i);
+	if (i>512)
+		die("Boot block may not exceed 512 bytes");
 	buf[510]=0x55;
 	buf[511]=0xAA;
 	i=write(1,buf,512);
@@ -55,13 +60,23 @@ int main(int argc, char ** argv)
 	
 	if ((id=open(argv[2],O_RDONLY,0))<0)
 		die("Unable to open 'system'");
-	if (read(id,buf,GCC_HEADER) != GCC_HEADER)
-		die("Unable to read header of 'system'");
-	if (((long *) buf)[5] != 0)
-		die("Non-GCC header of 'system'");
+	//if (read(id,buf,GCC_HEADER) != GCC_HEADER)
+	//	die("Unable to read header of 'system'");
+	//if (((long *) buf)[5] != 0)
+	//	die("Non-GCC header of 'system'");
 	for (i=0 ; (c=read(id,buf,sizeof buf))>0 ; i+=c )
 		if (write(1,buf,c)!=c)
 			die("Write call failed");
+
+	// pading to 512*n bytes
+	if (i % 512) {
+		memset(buf, 0, 1024);
+		pading = 512 - i % 512;
+		fprintf(stderr, "Pading %d bytes.\n", pading);
+		if (write(1, buf, pading) != pading)
+			die("Pading failed");
+	}
+
 	close(id);
 	fprintf(stderr,"System %d bytes.\n",i);
 	return(0);
